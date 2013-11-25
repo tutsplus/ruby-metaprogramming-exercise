24a5eb982999a7117fbf648cd33a93db263551a5
José Mota <jose@josemota.net>
2013-11-25 12:18:27 +0000

    Abstract module in test.

--- test/unit/meta_builder_test.rb
+++ test/unit/meta_builder_test.rb
@@ -1,40 +1,36 @@
 require "test_helper"
 require "metaproject/meta_builder"
 
-module MetaProject
-  describe MetaBuilder do
-    let(:dest) { "/tmp/project" }
-    let(:builder) { MetaBuilder.new }
+describe MetaProject::MetaBuilder do
+  let(:builder) { MetaProject::MetaBuilder.new }
 
-    it "spawns a new project" do
-      builder.project :blog
-      builder.execute
+  it "spawns a new project" do
+    builder.project :blog
+    builder.execute
 
-      Blog.must_be_kind_of Module
-    end
-
-    it "spawns a new class" do
-      builder.project :blog do
-        klass :post
-      end
-
-      builder.execute
-
-      Blog::Post.must_be_kind_of Class
-    end
-
-    it "spawns a new class with attributes" do
-      builder.project :blog do
-        klass :post do
-          attribute :title
-        end
-      end
-
-      builder.execute
-
-      post = Blog::Post.new "Title"
-      post.instance_variable_get(:@title).must_equal "Title"
-    end
+    Blog.must_be_kind_of Module
   end
 
+  it "spawns a new class" do
+    builder.project :blog do
+      klass :post
+    end
+
+    builder.execute
+
+    Blog::Post.must_be_kind_of Class
+  end
+
+  it "spawns a new class with attributes" do
+    builder.project :blog do
+      klass :post do
+        attribute :title
+      end
+    end
+
+    builder.execute
+
+    post = Blog::Post.new "Title"
+    post.instance_variable_get(:@title).must_equal "Title"
+  end
 end
